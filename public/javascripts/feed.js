0bb3f2a0ec4be62ad7aef50d8fd1e798f6254b69
Aaron Boyle <aaronboyle1@live.co.uk>
2018-04-05 23:27:52 +0100

    Getting there

--- public/javascripts/feed.js
+++ public/javascripts/feed.js
@@ -1,83 +1,130 @@
-$(document).ready(
-    function() {
-        var totalCharacters = 140;
-        var showPosts = true;
-        
-        $("#postForm").keyup(function (event) {
-            var inputText = event.target.value;
-            $("#charRemaining").html(totalCharacters - inputText.length);
-        });
-		getComments();
-        /**
-         * When the page loads (or is refreshed) we request all comments from the server
-         */
-        function getComments(){
+$(document).ready(function () {
+    var totalCharacters = 140;
+    var showPosts = true;
 
-            $.get( "/getComments", function( data ) {
-                var posts = "";
-                
-                for(var i=0; i<data.length; i++)
-                {
-                    posts += "<div class='well'><div class='row'><div class='col-xs-9'>"
-                        + data[i].comment + "</div><div class='col-xs-3'>" +
-                        "<button type='button' name='"+data[i]._id+"' class='btn btn-danger'>" 		    +"Delete</button></div></div></div></div>";
-                }
-                $( "#feedPosts" ).html( posts );
-                $( "#count" ).html(data.length);
-                if(!showPosts)
-                    $( "#feedPosts" ).hide();
-                else
-                    $( "#feedPosts" ).show();
-                
-                // Recursively call getComments every 10 seconds
-                setTimeout(getComments,10000);
-            });
-        }
-		
-		 /**
-		* Event handler for when the user submits a comment
-		*/
-        $("#postForm").submit(function (event) {
-            event.preventDefault();
+    /**
+     * Calculates the remaining characters and changes the html label with #charRemaining
+     */
+    $("#postForm").keyup(function (event) {
+        var inputText = event.target.value;
+        $("#charRemaining").html(totalCharacters - inputText.length);
+    });
+    getComments();
+
+    /**
+     * When the page loads (or is refreshed) we request all comments from the server
+     */
+    function getComments() {
+        $.get("/getComments", function(data) {
+            var posts = "";
+
+            for(var i = 0; i < data.length; i++) {
+                posts += "<div class='well'><div class='row'><div class='col-xs-9'>"
+                    + data[i].comment + "</div><div class='col-xs-1'><button type='button' name='" +
+                    data[i]._id + "' class='btn btn-success' id='e-btn'>Edit</button></div><div class='col-xs-1'>" +
+                    "<button type='button' name='" + data[i]._id + "' class='btn btn-danger' id='d-btn'>" + 
+                    "Delete</button></div></div></div></div>";
+            }
+
+            $("#feedPosts").html(posts);
+            $("#count").html(data.length);
+
+            if (!showPosts)
+                $("#feedPosts").hide();
+            else
+                $("#feedPosts").show();
+
+            // Recursively call getComments every 10 seconds
+            setTimeout(getComments, 10000);
+        });
+    }
+
+    /**
+   * Event handler for when the user submits a comment
+   */
+    $("#postForm").submit(function(event) {
+        event.preventDefault();
+
+        if(event.target.inputPost.value) {
             $.post("/addComment", {
-			comment: event.target.inputPost.value
+                comment: event.target.inputPost.value
             }, function (result) {
                 $("#charRemaining").html(totalCharacters);
                 event.target.reset();
                 getComments();
             });
+        } else {
+            $('[data-toggle="popover"]').popover(); //Toggles a popover on the post button
+        }
+    });
+
+    /**
+     * Determines which button to act on i.e. either the Edit or Delete button(MY VERSION)
+     */
+    $("#feedPosts").click(function(event) {
+        var name = event.target.name;   //Actually the comment id
+        var id = event.target.id;       //This is the button element id
+
+        if(name) {
+            if(id === "e-btn") {
+                updateComment(event);
+            }
+            else if(id === "d-btn") {
+                deleteComment(name);
+            }
+        }
+    });
+
+    /**
+     * Event handler for when the user edits a comment(MY VERSION)
+     */
+    function updateComment(event) {
+        var newComment;
+        var oldComment;
+        
+        $.get('/getComment/' + event.target.name, function(data) {
+            oldComment = data[0].comment;
         });
-		
-		/**
-         * Event handler for when the user deletes a comment
-         */
-        $("#feedPosts").click(function (event)
-        {
-            console.log(event.target.name);
-            if(event.target.name)
-            {
-                $.ajax({
-                url: '/removeComment/' + event.target.name,
-                type: 'DELETE',
+
+        
+
+        if(newComment) {
+            $.ajax({
+                url: '/updateComment/' + event.target.name,
+                type: 'PUT',
+                data: {comment: newComment},
                 success: function(result) {
+                    console.log('put in comment');
                     getComments();
                 }
             });
+        }   
+    }
+
+    /**
+     * Event handler for when the user deletes a comment
+     */
+    function deleteComment(name) {
+        $.ajax({
+            url: '/removeComment/' + name,
+            type: 'DELETE',
+            success: function(result) {
+                getComments();
             }
         });
-        
-        $("#btn-count").click(function (event) {
-            var options = {};
-            if(!showPosts)
-            {
-                $("#feedPosts").show( "blind", options, 1000);
-                showPosts = true;
-            }
-            else
-            {
-                $("#feedPosts").hide( "blind", options, 1000);
-                showPosts = false;
-            }
-        });
-	});
+    }
+
+    $("#btn-count").click(function(event) {
+        var options = {};
+
+        if (!showPosts) {
+            $("#feedPosts").show("blind", options, 1000);
+            showPosts = true;
+        }
+        else {
+            $("#feedPosts").hide("blind", options, 1000);
+            showPosts = false;
+        }
+    });
+});
 
--- routes/index.js
+++ routes/index.js
@@ -5,26 +5,27 @@ var jwt = require('jsonwebtoken');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Notezy' });
 });
 
 /* GET feed page. */
 router.get('/feed', function(req, res, next) {
-
-    try {
+    /*try {
         var jwtString = req.cookies.Authorization.split(" ");
         var profile = verifyJwt(jwtString[1]);
         if (profile) {
             res.render('feed');
         }
     }catch (err) {
-            res.json({
-                "status": "error",
-                "body": [
-                    "You are not logged in."
-                ]
-            });
-        }
+        res.json({
+            "status": "error",
+            "body": [
+                "You are not logged in."
+            ]
+        });
+    }*/
+
+    res.render('feed');
 });
 
 /**
@@ -34,7 +35,7 @@ router.post('/addComment', function(req, res, next) {
     // Extract the request body which contains the comments
     comment = new Comment(req.body);
     comment.save(function (err, savedComment) {
-        if (err)
+        if(err)
             throw err;
 
         res.json({
@@ -47,21 +48,36 @@ router.post('/addComment', function(req, res, next) {
  * Returns all comments from our database
  */
 router.get('/getComments', function(req, res, next) {
-
     Comment.find({}, function (err, comments) {
         if (err)
             res.send(err);
+
         res.json(comments);
     });
+});
+
+/**
+ * Returns a single comment from the database(MY VERSION)
+ */
+router.get('/getComment/:id', function(req, res, next) {
+    var id = req.params.id;
+
+    Comment.find({_id: id}, function(err, comment) {
+        if(err){
+            throw err;
+        }
+
+        res.json(comment);
+    })
 });
 
 /**
   Updates a comment already in the database
  */
 router.put('/updateComment/:id', function(req, res, next){
-
     var id = req.params.id;
-    Comment.update({_id:id}, req.body, function (err) {
+
+    Comment.update({_id: id}, req.body, function (err) {
         if (err)
             res.send(err);
 
@@ -73,8 +89,8 @@ router.put('/updateComment/:id', function(req, res, next){
  * Deletes a comment from the database
  */
 router.delete('/removeComment/:id', function(req, res, next){
-
     var id = req.params.id;
+
     Comment.remove({_id:id}, function (err) {
         if (err)
             res.send(err);
